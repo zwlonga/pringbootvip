81cd46f186481c848bfb0df30d3f4f6889a8b2c0
unknown <513707474@qq.com>
2020-06-01 22:30:05 +0800

    v0.1

---
+++ src/main/java/cn/enjoy/App.java
@@ -0,0 +1,24 @@
+package cn.enjoy;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@MapperScan("cn.enjoy.dao")
+@EnableTransactionManagement
+public class App {
+
+    public static void main(String[] args) {
+        //启动类
+        //定制化
+        SpringApplication.run(App.class, args);
+    }
+}
