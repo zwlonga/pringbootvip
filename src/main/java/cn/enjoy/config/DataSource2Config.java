100ce84cb1dd3068c0d6aa646a6dfffc5151aa6a
51370 <513707474@qq.com>
2020-06-25 12:16:33 +0800

    test

---
+++ src/main/java/cn/enjoy/config/DataSource2Config.java
@@ -0,0 +1,47 @@
+package cn.enjoy.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@MapperScan(basePackages = "cn.enjoy.dao.orders", sqlSessionFactoryRef = "test2SqlSessionFactory")
+public class DataSource2Config {
+    @Bean(name = "test2DataSource")
+    @ConfigurationProperties(prefix = "spring.datasource2")
+    public DataSource testDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "test2SqlSessionFactory")
+    public SqlSessionFactory testSqlSessionFactory(@Qualifier("test2DataSource") DataSource dataSource)
+            throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+         bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapping/orders/*.xml"));
+        return bean.getObject();
+    }
+
+    @Bean(name = "test2TransactionManager")
+    public DataSourceTransactionManager testTransactionManager(@Qualifier("test2DataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "test2SqlSessionTemplate")
+    public SqlSessionTemplate testSqlSessionTemplate(
+            @Qualifier("test2SqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
---
+++ src/main/java/cn/enjoy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package cn.enjoy.service.impl;
+
+import cn.enjoy.dao.orders.OrdersMapper;
+import cn.enjoy.dao.users.UsersMapper;
+import cn.enjoy.model.Orders;
+import cn.enjoy.model.Users;
+import cn.enjoy.service.IOrderService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class OrderServiceImpl  implements IOrderService{
+
+    @Resource
+    private UsersMapper usersMapper;
+
+    @Resource
+    private OrdersMapper ordersMapper;
+
+    @Override
+    public void addOrder(Orders orders, Users users) {
+        usersMapper.insertSelective(users);
+        ordersMapper.insertSelective(orders);
+    }
+}
